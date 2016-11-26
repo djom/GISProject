49685e4f65df33b12c7ba065d224106036025a98
djom <dusan.jom@gmail.com>
2016-11-26 20:21:55 +0100

    [1] First commit

---
+++ GISApplication/PgSqlConnectiona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Npgsql;
+using System.Configuration;
+
+namespace GISApplication
+{
+    public class PgSqlConnection
+    {
+        private static NpgsqlConnection sqlCon = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["PGSQL"].ConnectionString);
+        public PgSqlConnection() { }
+        public static void GetMeSomething()
+        {
+            sqlCon.Open();
+        }
+    }
+}
---
+++ GISApplication/index.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="GISApplication.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <script type="text/javascript" src="Scripts/RestManager.js"></script>
+    <script type="text/javascript" src="Scripts/jquery-1.8.3.min.js"></script>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Dušan
+        </div>
+    </form>
+</body>
+</html>
